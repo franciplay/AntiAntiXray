30a3b38f7de92176db3b0eecba1a9b425b928e92
franciplay58 <fla.for79@gmail.com>
2026-05-20 14:41:20 +0200

    Revert "fix: progress UI toast being stuck by changing it to actionbar message"
    This reverts commit 7709b4eb3ba3614bc8a6b2dc06f0a078ec2657e6.

--- src/main/java/me/constantindev/antiantixray/GUI/ProgressBar.java
+++ src/main/java/me/constantindev/antiantixray/GUI/ProgressBar.java
@@ -2,6 +2,7 @@ package me.constantindev.antiantixray.GUI;
 
 import me.constantindev.antiantixray.Etc.Config;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.network.chat.Component;
 
 public class ProgressBar {
@@ -15,19 +16,21 @@ public class ProgressBar {
         this.todo = Math.pow(rad * 2 + 1, 3);
     }
 
+    private static final SystemToast.SystemToastId TOAST_ID = new SystemToast.SystemToastId(10000L); // 10s default
+
     private static double round(double value) {
         int scale = (int) Math.pow(10, 2);
         return (double) Math.round(value * scale) / scale;
     }
 
     public void updateDisplay() {
-        if (!done) {
+        if (done) {
+            SystemToast.forceHide(Minecraft.getInstance().getToastManager(), TOAST_ID);
+        } else {
             String text = round((progress / todo) * 100) + "%";
             if (!text.equals(lastText)) {
                 lastText = text;
-                if (Minecraft.getInstance().player != null) {
-                    Minecraft.getInstance().player.sendOverlayMessage(Component.literal("Refreshing blocks: " + text));
-                }
+                SystemToast.addOrUpdate(Minecraft.getInstance().getToastManager(), TOAST_ID, Component.literal("Refreshing blocks..."), Component.literal(text));
             }
         }
     }
